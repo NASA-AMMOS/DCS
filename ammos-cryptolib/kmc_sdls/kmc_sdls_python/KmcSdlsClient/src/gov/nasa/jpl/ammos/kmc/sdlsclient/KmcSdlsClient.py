a81daa2cafe02fc1f7c996bab1d6fea6ef5bfb40
JP Pan (393C) <panjames@jpl.nasa.gov>
2026-08-06 16:57:25 +0000

    fix max frame length error messages

--- ammos-cryptolib/kmc_sdls/kmc_sdls_python/KmcSdlsClient/src/gov/nasa/jpl/ammos/kmc/sdlsclient/KmcSdlsClient.py
+++ ammos-cryptolib/kmc_sdls/kmc_sdls_python/KmcSdlsClient/src/gov/nasa/jpl/ammos/kmc/sdlsclient/KmcSdlsClient.py
@@ -73,10 +73,10 @@ def get_max_frame_size(type, input_byte_array) -> int:
     if frame_key in frame_global_config and "max_frame_length" in frame_global_config[frame_key]:
         max_frame_size = frame_global_config[frame_key]["max_frame_length"]
     else:
-        raise SdlsClientException(SdlsClientException.SDLS_INITIALIZATION_ERROR, f"max_frame_length must be configured for {type}.{tfvn}.{scid}.{vcid}")
+        raise SdlsClientException(SdlsClientException.SDLS_INITIALIZATION_ERROR, f"max_frame_length must be configured for {frame_key}")
 
     if len(input_byte_array) > max_frame_size:
-        raise SdlsClientException(SdlsClientException.PROCESS_SECURITY_EXCEPTION, f"AssertionError: 'Input frame exceeds max frame size. {len(input_byte_array)} bytes is larger than configured maximum {max_frame_size} bytes for {type}.{tfvn}.{scid}.{vcid}'")
+        raise SdlsClientException(SdlsClientException.PROCESS_SECURITY_EXCEPTION, f"AssertionError: 'Input frame exceeds max frame size. {len(input_byte_array)} bytes is larger than configured maximum {max_frame_size} bytes for {frame_key}'")
 
     return max_frame_size
 
